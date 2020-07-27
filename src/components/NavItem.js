0286ccbb4ebee0268990907a18c45583e3c2f8bb
Kok Kok <REMY@Koks-MacBook-Pro.local>
2020-07-27 19:43:24 -0400

    this is a  commit

---
+++ src/components/NavItem.js
@@ -0,0 +1,11 @@
+
+
+function NavItem(props){
+    return (
+        <li className="nav-item">
+            <a href="#" classNmae="icon-button">
+                
+            </a>
+        </li>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function Navbar(props) {
+    return (
+        <nav className="navbar">
+            <ul className="navbar-nav">
+
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
